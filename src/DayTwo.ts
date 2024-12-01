260baddf12112cb41b198cd1d7e1371d9a7f56c0
jacobanderson <jacobAnderson931@gmail.com>
2024-11-30 22:43:43 -0700

    Day two setup

--- src/DayTwo.ts
+++ src/DayTwo.ts
@@ -7,24 +7,26 @@ const actualLines:string[] = actualFile.split("\n");
 const sampleFile:string = readFileSync('Input/'+filename2, 'utf-8');
 const sampleLines:string[] = sampleFile.split("\n");
 
-console.log('sample p1: ' + partOne(sampleFile, sampleLines));
-console.log('actual p1: ' + partOne(actualFile, actualLines));
-console.log('sample p2: ' + partTwo(sampleFile, sampleLines));
-console.log('actual p2: ' + partTwo(actualFile, actualLines));
+console.log('sample p1: ');
+partOne(sampleFile, sampleLines);
+console.log('actual p1: ');
+partOne(actualFile, actualLines);
+console.log('sample p2: ');
+partTwo(sampleFile, sampleLines);
+console.log('actual p2: ');
+partTwo(actualFile, actualLines);
 
 
-
-function partOne(file:string, lines:string[]): number{
+function partOne(file:string, lines:string[]){
 
     lines.forEach((line:string, index:number)=>{
 
     });
-    return NaN
 }
-function partTwo(file:string, lines:string[]): number{
+function partTwo(file:string, lines:string[]){
 
     lines.forEach((line:string, index:number)=>{
 
     });
-    return NaN
+
 }
