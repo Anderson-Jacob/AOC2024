b14f4c5f777d23e52ac5132375f9000e81953d20
jacobanderson <jacobAnderson931@gmail.com>
2024-11-30 22:13:40 -0700

    Day one

--- src/DayOne.ts
+++ src/DayOne.ts
@@ -1,8 +1,27 @@
 import {readFileSync} from "node:fs";
+const filename = 'actual-input.txt';
+//const filename = 'sample.txt';
 
-const file:string = readFileSync('Input/sample.txt', 'utf-8');
-let lines:string[] = file.split("\n");
 
+const file:string = readFileSync('Input/'+filename, 'utf-8');
+const lines:string[] = file.split("\n");
+let map = new Map<number, number>();
+
+let left:number[] = [];
 lines.forEach((line:string, index:number)=>{
-    console.log(line)
+    const sides = line.split(" ")
+    let l = Number(sides[0])
+    let r = Number(sides[3])
+    left.push(l)
+    if(!map.has(r)){
+        map.set(r, 1)
+    }else{
+        map.set(r, map.get(r)+1)
+    }
 });
+let total = 0;
+for(let i = 0; i<left.length; i++){
+    if(map.has(left[i]))
+        total+= left[i] * map.get(left[i])
+}
+console.log(total)
