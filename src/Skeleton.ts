14611f33a9318658f21a8c65edfc5063a5a5e8bf
jacobanderson <jacobAnderson931@gmail.com>
2024-12-06 22:43:28 -0700

    Reformat skeleton file

--- src/Skeleton.ts
+++ src/Skeleton.ts
@@ -1,30 +1,32 @@
 import {readFileSync} from "node:fs";
+
 const filename = 'actual-input.txt';
 const filename2 = 'sample.txt';
 
-const actualFile:string = readFileSync('Input/'+filename, 'utf-8');
-const actualLines:string[] = actualFile.split("\n");
-const sampleFile:string = readFileSync('Input/'+filename2, 'utf-8');
-const sampleLines:string[] = sampleFile.split("\n");
+const actualFile: string = readFileSync(`Input/${filename}`, 'utf-8');
+const actualLines: string[] = actualFile.split("\n");
+const sampleFile: string = readFileSync(`Input/${filename2}`, 'utf-8');
+const sampleLines: string[] = sampleFile.split("\n");
 
 console.log('sample p1: ');
 partOne(sampleFile, sampleLines);
 console.log('actual p1: ');
 partOne(actualFile, actualLines);
+
 console.log('sample p2: ');
 partTwo(sampleFile, sampleLines);
 console.log('actual p2: ');
 partTwo(actualFile, actualLines);
 
 
-function partOne(file:string, lines:string[]){
-    lines.forEach((line:string, index:number)=>{
-        console.log(line.split(/(\s+)/).filter( e => e.trim().length > 0))
+function partOne(file: string, lines: string[]) {
+    lines.forEach((line: string, index: number) => {
+        console.log(line.split(/(\s+)/).filter(e => e.trim().length > 0))
     });
 }
-function partTwo(file:string, lines:string[]){
 
-    lines.forEach((line:string, index:number)=>{
+function partTwo(file: string, lines: string[]) {
+    lines.forEach((line: string, index: number) => {
 
     });
 
